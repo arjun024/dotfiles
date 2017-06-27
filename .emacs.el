8e97d36c932e462aea397ec580e61ce21b03a681
Arjun Sreedharan <arjun024@gmail.com>
2017-06-27 17:00:25 -0400

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,3 +1,10 @@
+; Highlight tabs and trailing whitespace everywhere
+(setq whitespace-style '(face trailing tabs))
+(custom-set-faces
+ '(whitespace-tab ((t (:background "red")))))
+(global-whitespace-mode)
+
+;Theme stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,4 +21,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo")))))
- 
+
+; line numbers on the left side
+(global-linum-mode t)
+; Don't show welcome to emacs screen
+(setq inhibit-startup-screen t)
